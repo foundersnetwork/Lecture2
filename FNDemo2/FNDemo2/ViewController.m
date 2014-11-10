823712369978f5d943fc92b0a2c2e586a57ae721
Mark Hall <mhall48@uwo.ca>
2014-11-10 15:41:28 -0500

    updated the comments a bit

--- FNDemo2/FNDemo2/ViewController.m
+++ FNDemo2/FNDemo2/ViewController.m
@@ -19,7 +19,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     //Declare your array of names here
-    //Hint: _namesArray=@[........]
+    //Hint: _namesArray=@[<insert strings of names here>]
     
 }
 - (IBAction)searchPressed:(id)sender {
@@ -27,6 +27,7 @@
     //Get the text in the textfield
     //Loop through the array and check to see if it is in there
     //Use a BOOL to say whether the name is found or not
+    //And when the loop is done, set the text of _outputField to display if the name is found
 }
 
 @end
